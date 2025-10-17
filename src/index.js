dd91a60c22f13d1bb44ea3a0a33d6e878d57a532
shaw-samina <jinxcapsule@yahoo.com>
2025-10-17 18:55:28 -0400

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function updateWeather(response) {
+  let temperature = document.querySelector("#current-temp");
+  let currentTemp = response.data.temperature.current;
+  temperature.innerHTML = Math.round(currentTemp);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "f0a4tbb1fc986b4d534ccb04b8291ao0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let enterCityBox = document.querySelector("#search-form-input");
-  let city = document.querySelector("#city");
-  city.innerHTML = enterCityBox.value;
+
+  searchCity(enterCityBox.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Chicago");
