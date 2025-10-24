3a4723485b82d2bcbd052b9d4c46725a2f1f9c04
shaw-samina <jinxcapsule@yahoo.com>
2025-10-23 23:44:52 -0400

    Added forecast API integration, formatted the timestamp to display weekday. restyled forecast.

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ function updateWeather(response) {
   dayAndTime.innerHTML = formatDate(date);
   let icon = document.querySelector("#icon");
   icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-emoji" />`;
+  getForecast(response.data.city);
 }
 function formatDate(date) {
   let days = [
@@ -48,23 +49,42 @@ function handleSearchSubmit(event) {
 
   searchCity(enterCityBox.value);
 }
+function formatDay(timestamp) {
+  let day = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day.getDay()];
+}
 
-function showForecast() {
+function getForecast(city) {
+  let apiKey = "f0a4tbb1fc986b4d534ccb04b8291ao0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showForecast);
+}
+
+function showForecast(response) {
   let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
   let forecastLoop = "";
 
-  days.forEach(function (day) {
-    forecastLoop =
-      forecastLoop +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastLoop =
+        forecastLoop +
+        `
           <div class="weather-forecast-days">
-            <div class="forecast-day">${day}</div>
-            <div class="forecast-icon">🌤️</div>
+            <div class="forecast-day">${formatDay(day.time)}</div>
+            <div class="forecast-icon">
+            <img src="${day.condition.icon_url}" />
+            </div>
             <div class="forecast-temps">
-              <div class="forecast-temp"><strong>60°</strong></div>
-              <div class="forecast-temp">82°</div>
+              <div class="forecast-temp"><strong>${Math.round(
+                day.temperature.maximum
+              )}</strong></div>
+              <div class="forecast-temp">${Math.round(
+                day.temperature.minimum
+              )}</div>
             </div>
           </div>  `;
+    }
   });
   let forecast = document.querySelector("#forecast");
   forecast.innerHTML = forecastLoop;
@@ -74,4 +94,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Chicago");
-showForecast();
